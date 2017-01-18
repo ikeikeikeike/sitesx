05c35d3b308d38468c0bc854988fea9ba00269a8
ikeikeikeike <jp.ne.co.jp@gmail.com>
2017-01-19 00:15:56 +0900

    bugfix

--- lib/sitesx/plug.ex
+++ lib/sitesx/plug.ex
@@ -5,10 +5,12 @@ defmodule Sitesx.Plug do
 
   def init(opts), do: opts
   def call(conn, opts) do
+    site = Module.concat cfg_app, Site
+
     with qs when not is_nil(qs) <- Sitesx.Q.findsite(conn),
-         %cfg_app.Site{} = site <- Module.concat(cfg_app, Repo).one(qs)
+         struct(site) = model   <- Module.concat(cfg_app, Repo).one(qs)
     do
-           put_private conn, :sitesx_model, site
+           put_private conn, :sitesx_model, model
     else
       _ -> put_private conn, :sitesx_model, nil
     end
